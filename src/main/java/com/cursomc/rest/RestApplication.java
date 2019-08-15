ed95d7fec284723ae9163474d23e72a64c442a09
Davi Holanda <daviht7@gmail.com>
2019-08-15 20:11:33 -0300

    rest h2

--- src/main/java/com/cursomc/rest/RestApplication.java
+++ src/main/java/com/cursomc/rest/RestApplication.java
@@ -1,13 +1,13 @@
-package com.cursomc.demo;
+package com.cursomc.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class RestApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(RestApplication.class, args);
 	}
 
 }
--- src/main/java/com/cursomc/rest/resource/CategoriaResource.java
+++ src/main/java/com/cursomc/rest/resource/CategoriaResource.java
@@ -1,4 +1,4 @@
-package com.cursomc.demo.resource;
+package com.cursomc.rest.resource;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
